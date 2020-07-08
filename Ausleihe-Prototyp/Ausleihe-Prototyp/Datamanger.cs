910e627d99eb0ae16488c074e05d21e4f968a3c4
aomari99 <52887341+aomari99@users.noreply.github.com>
2020-07-08 18:33:12 +0200

    datamanger plus startseite update

---
+++ Ausleihe-Prototyp/Ausleihe-Prototyp/Datamanger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ausleihe_Prototyp
+{
+   public static class Datamanger
+    {
+      
+
+        public static List<Ausleihe> Ausleihen = new List<Ausleihe>();
+        public static List<Student> Studenten = new List<Student>();
+        public static List<Transponder> Transponders = new List<Transponder>();
+        public static void init()
+        {
+
+        }
+
+    }
+}
