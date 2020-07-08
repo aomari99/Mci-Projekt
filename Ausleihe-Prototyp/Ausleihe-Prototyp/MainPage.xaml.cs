9723c93688c2e5ff22d3404ccc6f6ae8c79c9c47
Hountrezz <lukazappel@gmail.com>
2020-07-08 17:04:19 +0200

    Login done

--- Ausleihe-Prototyp/Ausleihe-Prototyp/MainPage.xaml.cs
+++ Ausleihe-Prototyp/Ausleihe-Prototyp/MainPage.xaml.cs
@@ -26,10 +26,5 @@ namespace Ausleihe_Prototyp
         {
             this.InitializeComponent();
         }
-
-        private void textBox_username_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
