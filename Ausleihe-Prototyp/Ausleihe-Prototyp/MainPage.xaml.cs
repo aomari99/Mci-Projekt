fa60b7f0ac07bb809e54f2bebe4761fb3b965cd7
Hountrezz <lukazappel@gmail.com>
2020-07-08 17:44:03 +0200

    Added login functionality

--- Ausleihe-Prototyp/Ausleihe-Prototyp/MainPage.xaml.cs
+++ Ausleihe-Prototyp/Ausleihe-Prototyp/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,9 +29,18 @@ namespace Ausleihe_Prototyp
             this.InitializeComponent();
         }
 
-        private void button_Login_Click(object sender, RoutedEventArgs e)
+        private async void button_Login_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(Info));
-        }
+            Debug.WriteLine(passwordBox_password.Password);
+            string pw = passwordBox_password.Password;
+            string user = textBox_username.Text;
+            if (pw == "12345" && user == "ogate"){
+                Frame.Navigate(typeof(Info));
+            }
+            else {
+                var message = new MessageDialog("Login fehlgeschlagen");
+                await message.ShowAsync();
+            }
+        }   
     }
 }
