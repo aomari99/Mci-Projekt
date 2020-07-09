39c80e87b8675735f7f6a881465292f992be5a8c
aomari99 <52887341+aomari99@users.noreply.github.com>
2020-07-09 16:38:39 +0200

    tabelle

--- Ausleihe-Prototyp/Ausleihe-Prototyp/Ausgeliehene_Transponder.xaml.cs
+++ Ausleihe-Prototyp/Ausleihe-Prototyp/Ausgeliehene_Transponder.xaml.cs
@@ -3,11 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,68 +29,195 @@ namespace Ausleihe_Prototyp
     public sealed partial class Ausgeliehene_Transponder : Page
     {
 
-        List<Ausleihe> nochda = new List<Ausleihe>();
+         
 
-        List<Ausleihe> datamangerlocal  = Datamanger.Ausleihen;
+         
 
 
-        private ObservableCollection<Ausleihe> Items;
+        public ObservableCollection<data> collection = new ObservableCollection<data>();
         public Ausgeliehene_Transponder()
         {
 
-           
-        
 
+
+            this.DataContext = this;
             this.InitializeComponent();
 
-
-
-            DataTable dt = GetDataTable();
            
-            FillDataGrid(dt, MyDataGrid);
-        }
 
-        private static DataTable GetDataTable()
-        {
-            DataTable dt = new DataTable();
-
-            dt.Columns.Add("Vorname", typeof(string));
-            dt.Columns.Add("Nachname", typeof(string));
-            dt.Columns.Add("Matrikelnummer", typeof(string));
-            dt.Columns.Add("Transpondernummer", typeof(int));
-            dt.Columns.Add("Ausgeliehen am", typeof(string));
+            // DataTable dt = GetDataTable();
 
             foreach (var x in Datamanger.Ausleihen)
             {
                 if (x.abegegeben == false)
-                    dt.Rows.Add(x.Student.Vorname ,x.Student.Nachname , x.Student.Matrikelnummer , x.Transponder.Transpondernummer , x.Ausgeliehenam);
+                {
+
+
+
+                    collection.Add(new data(x.Student.Vorname, x.Student.Nachname, x.Student.Matrikelnummer, x.Transponder.Transpondernummer, x.Ausgeliehenam));
+                }
             }
-            return dt;
+
+          //  MyDataGrid.ItemsSource = collection;
+            // FillDataGrid(dt, MyDataGrid);
         }
 
 
 
 
-        public static void FillDataGrid(DataTable table, DataGrid grid)
+        public class data {
+          public  string Vorname { get; set; }
+            public string Nachname { get; set; }
+            public string Matrikelnummer { get; set; }
+            public int Transpondernummer { get; set; }
+            public string Ausgeliehenam { get; set; }
+
+            public data(string a, string b , string c , int d , string e)
+            {
+                Vorname = a; Nachname = b; Matrikelnummer = c; Transpondernummer = d; Ausgeliehenam = e;
+
+            }
+            }
+
+   
+        
+   
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            grid.Columns.Clear();
-            grid.AutoGenerateColumns = false;
-            for (int i = 0; i < table.Columns.Count; i++)
+            string mtrnummer = ((Button)sender).Tag.ToString();
+            Debug.WriteLine(mtrnummer);
+            foreach (var x in Datamanger.Ausleihen)
             {
-                grid.Columns.Add(new DataGridTextColumn()
+                if (x.abegegeben == false && x.Student.Matrikelnummer == mtrnummer)
                 {
-                    Header = table.Columns[i].ColumnName,
-                    Binding = new Binding { Path = new PropertyPath("[" + i.ToString() + "]") }
-                });
+                    x.abegegeben = true;
+                }
             }
-
-            var collection = new ObservableCollection<object>();
-            foreach (DataRow row in table.Rows)
+            collection.Clear();
+            foreach (var x in Datamanger.Ausleihen)
             {
-                collection.Add(row.ItemArray);
+                if (x.abegegeben == false)
+                {
+
+                  
+
+                    collection.Add(new data(x.Student.Vorname, x.Student.Nachname, x.Student.Matrikelnummer, x.Transponder.Transpondernummer, x.Ausgeliehenam));
+                }
+            }
+        }
+        public string lasttag = "";
+        private void MyDataGrid_Sorting(object sender, DataGridColumnEventArgs e)
+        {
+
+
+            // Remove sorting indicators from other columns
+            try
+            {
+                foreach (var dgColumn in MyDataGrid.Columns)
+                {
+                    if (dgColumn.Tag != e.Column.Tag)
+                    {
+                        dgColumn.SortDirection = null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
             }
 
-            grid.ItemsSource = collection;
+
+            //Use the Tag property to pass the bound column name for the sorting implementation 
+            if (e.Column.Tag.ToString() == "Nachname")
+            {
+                //Implement sort on the column "Range" using LINQ
+                if (e.Column.SortDirection == null || e.Column.SortDirection == DataGridSortDirection.Descending)
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                        orderby item.Nachname ascending
+                                                                        select item);
+                    e.Column.SortDirection = DataGridSortDirection.Ascending;
+                }
+                else
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                        orderby item.Nachname descending
+                                                                        select item);
+                    e.Column.SortDirection = DataGridSortDirection.Descending;
+                }
+            }else if (e.Column.Tag.ToString() == "Vorname")
+            {
+                //Implement sort on the column "Range" using LINQ
+                if (e.Column.SortDirection == null || e.Column.SortDirection == DataGridSortDirection.Descending)
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                            orderby item.Vorname ascending
+                                                                            select item);
+                    e.Column.SortDirection = DataGridSortDirection.Ascending;
+                }
+                else
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                            orderby item.Vorname descending
+                                                                            select item);
+                    e.Column.SortDirection = DataGridSortDirection.Descending;
+                }
+            }
+            else if (e.Column.Tag.ToString() == "Matrikelnummer")
+            {
+                //Implement sort on the column "Range" using LINQ
+                if (e.Column.SortDirection == null || e.Column.SortDirection == DataGridSortDirection.Descending)
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                            orderby item.Matrikelnummer ascending
+                                                                            select item);
+                    e.Column.SortDirection = DataGridSortDirection.Ascending;
+                }
+                else
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                            orderby item.Matrikelnummer descending
+                                                                            select item);
+                    e.Column.SortDirection = DataGridSortDirection.Descending;
+                }
+            }
+            else if (e.Column.Tag.ToString() == "Transpondernummer")
+            {
+                //Implement sort on the column "Range" using LINQ
+                if (e.Column.SortDirection == null || e.Column.SortDirection == DataGridSortDirection.Descending)
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                            orderby item.Transpondernummer ascending
+                                                                            select item);
+                    e.Column.SortDirection = DataGridSortDirection.Ascending;
+                }
+                else
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                            orderby item.Transpondernummer descending
+                                                                            select item);
+                    e.Column.SortDirection = DataGridSortDirection.Descending;
+                }
+            }
+            else if (e.Column.Tag.ToString() == "Ausgeliehenum")
+            {
+                //Implement sort on the column "Range" using LINQ
+                if (e.Column.SortDirection == null || e.Column.SortDirection == DataGridSortDirection.Descending)
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                            orderby item.Ausgeliehenam ascending
+                                                                            select item);
+                    e.Column.SortDirection = DataGridSortDirection.Ascending;
+                }
+                else
+                {
+                    MyDataGrid.ItemsSource = new ObservableCollection<data>(from item in collection
+                                                                            orderby item.Ausgeliehenam descending
+                                                                            select item);
+                    e.Column.SortDirection = DataGridSortDirection.Descending;
+                }
+            }
+
         }
     }
 }
